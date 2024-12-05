76142cdd2607d3700a1b1986cc1f3988b9c4847d
pavan-ki <pavan.ki@hotmail.com>
2024-12-05 14:20:32 +0000

    updates

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ function populateMenu() {
         "Chapter 06 - IT APPLICATIONS IN MOTOR INSURANCE": ["Easy.json", "Medium.json", "Hard.json"],
         "Chapter 07 - CONSUMER DELIGHT": ["Easy.json", "Medium.json", "Hard.json"],
         "Chapter 08 - THIRD PARTY LIABILITY INSURANCE": ["Easy.json", "Medium.json", "Hard.json"],
-        "Chapter 09 - PROCEDURES FOR FILING AND DEFENDING": ["Easy.json", "Medium.json", "Hard.json"]
+        "Chapter 09 - PROCEDURES FOR FILING AND DEFENDING": ["Easy.json", "Medium.json", "Hard.json"],
+        "Chapter 10 - QUANTUM FIXATION": ["Easy.json", "Medium.json", "Hard.json"]  
     };
 
     const sidenav = document.getElementById("mySidenav");
